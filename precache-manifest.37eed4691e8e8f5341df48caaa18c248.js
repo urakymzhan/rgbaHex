0518f50b4cb031e45a767a0d99db4dc64aeab0b4
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-07-03 02:27:20 -0700

    Updates

---
+++ precache-manifest.37eed4691e8e8f5341df48caaa18c248.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb48b94f66cdb5c931a1",
+    "url": "/rgbaHex/static/css/main.fb87aa5a.chunk.css"
+  },
+  {
+    "revision": "bb48b94f66cdb5c931a1",
+    "url": "/rgbaHex/static/js/main.eba692b0.chunk.js"
+  },
+  {
+    "revision": "7668020cb09b7d00127d",
+    "url": "/rgbaHex/static/js/runtime~main.69168d73.js"
+  },
+  {
+    "revision": "6eb18821f6fbd9ec3fc0",
+    "url": "/rgbaHex/static/css/2.30653d25.chunk.css"
+  },
+  {
+    "revision": "6eb18821f6fbd9ec3fc0",
+    "url": "/rgbaHex/static/js/2.114820c7.chunk.js"
+  },
+  {
+    "revision": "21d4c369798340ed27f3afcc80d8a27c",
+    "url": "/rgbaHex/static/media/home.21d4c369.png"
+  },
+  {
+    "revision": "5387cd43cbc354e61f1c75497547de38",
+    "url": "/rgbaHex/static/media/bg2.5387cd43.svg"
+  },
+  {
+    "revision": "0e5dd99c883f73fdbccb5a9124a62c67",
+    "url": "/rgbaHex/index.html"
+  }
+];
